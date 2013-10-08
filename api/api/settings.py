19cc365a5b2062860666888bd584cacd92cb3bf5
Tharshan <tharshan09@gmail.com>
2013-10-08 17:25:00 +0100

    add Procfile and requirements + cleanup

--- api/api/settings.py
+++ api/api/settings.py
@@ -1,5 +1,6 @@
 # Django settings for api project.
-
+import dj_database_url
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -153,3 +154,23 @@ LOGGING = {
         },
     }
 }
+
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
